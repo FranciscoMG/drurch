1a2d98ea99b690c2f283640952037df5b1ecfbc4
FranciscoMG <franciscojavier.melendez@ucr.ac.cr>
2017-05-08 21:34:06 -0600

    Diseño del login completo y el registro

---
+++ app/src/main/java/com/drurch/actRegistro.java
@@ -0,0 +1,13 @@
+package com.drurch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class actRegistro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_registro);
+    }
+}
