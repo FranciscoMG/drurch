28fb0fde4b425560fcaf0d0c8be6a9aaef717a93
FranciscoMG <franciscojavier.melendez@ucr.ac.cr>
2017-05-09 22:41:30 -0600

    Terminados diseños de algunos layouts

--- app/src/main/java/com/drurch/actMapa.java
+++ app/src/main/java/com/drurch/actMapa.java
@@ -1,14 +1,12 @@
 package com.drurch;
 
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
