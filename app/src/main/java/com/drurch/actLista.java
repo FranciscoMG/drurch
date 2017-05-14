60153197331a91c6e4091672b2bdcc3196b3161e
FranciscoMG <franciscojavier.melendez@ucr.ac.cr>
2017-05-14 11:25:56 -0600

    Terminados diseños de algunos layouts

--- app/src/main/java/com/drurch/actLista.java
+++ app/src/main/java/com/drurch/actLista.java
@@ -13,8 +13,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class actLista extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class actLista extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    protected  DrawerLayout dlOpciones;
+    protected ActionBarDrawerToggle adtLateral;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +24,15 @@ public class actLista extends AppCompatActivity
         setContentView(R.layout.act_lista);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
 
+        dlOpciones = (DrawerLayout) findViewById(R.id.drawer_layout);
+        adtLateral = new ActionBarDrawerToggle(this, dlOpciones, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        adtLateral.setDrawerIndicatorEnabled(true);
+        dlOpciones.addDrawerListener(adtLateral);
+        adtLateral.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
@@ -32,34 +40,11 @@ public class actLista extends AppCompatActivity
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
+        if (dlOpciones.isDrawerOpen(GravityCompat.START)) {
+            dlOpciones.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.act_lista, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
@@ -82,8 +67,7 @@ public class actLista extends AppCompatActivity
 
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
+        dlOpciones.closeDrawer(GravityCompat.START);
         return true;
     }
 }
